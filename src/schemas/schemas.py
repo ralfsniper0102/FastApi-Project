d60b002a70dfd6d63286938dbdff73a164dd3133
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-08-18 20:54:54 -0300

    feat: validation in PostCreateRequest and UserCreateViewModel / refactor: schemas

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator 
 import datetime
+from src.validations.patterns import validName, validEmail, validPassword
 
 class GetAllUsersResponseModel(BaseModel):
     name: str
@@ -9,20 +10,39 @@ class GetAllUsersResponseModel(BaseModel):
     class Config:
         from_attributes = True
 
-class UserViewModel(BaseModel):
-    id: int
+class PostCreateRequest(BaseModel):
     name: str
     email: str
     birthDay: datetime.date
+    password: str
+    
+    @field_validator('name')
+    def checkName(cls, value):
+        if len(value) < 3:
+            raise ValueError('Nome deve conter mais de 3 caracteres')
+        if not validName(value):
+            raise ValueError('Nome inválido')
+        return value
+    
+    @field_validator('email')
+    def checkEmail(cls, value):
+        if not validEmail(value):
+            raise ValueError('Email inválido')
+        return value
+
+    @field_validator('password')
+    def checkPassword(cls, value):
+        if not validPassword(value):
+            raise ValueError('Senha inválida')
+        return value
     class Config:
         from_attributes = True
 
-class UserCreate(BaseModel):
+class GetByIdResponse(BaseModel):
     name: str
     email: str
     password: str
     birthDay: datetime.date
-   
     class Config:
         from_attributes = True
 
@@ -30,5 +50,25 @@ class LoginRequest(BaseModel):
     email: str
     password: str
     
+    @field_validator('email')
+    def checkEmail(cls, value):
+        if not validEmail(value):
+            raise ValueError('Email inválido')
+        return value
+
+    @field_validator('password')
+    def checkPassword(cls, value):
+        if not validPassword(value):
+            raise ValueError('Senha inválida')
+        return value
+    
 class LoginResponse(BaseModel):
     pass
+
+class UserCreateViewModel(BaseModel):
+    name: str
+    email: str
+    birthDay: datetime.date
+    password: str
+    class Config:
+        from_attributes = True
